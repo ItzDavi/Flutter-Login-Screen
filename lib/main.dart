54b9c5f7d6cfad37c442d7b4b3c6ff38a61bcfd9
Davide <69810644+ItzDavi@users.noreply.github.com>
2022-09-22 09:54:56 +0200

    Added tap detection on screen

--- lib/main.dart
+++ lib/main.dart
@@ -87,135 +87,150 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text(widget.title),
-        elevation: 0
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 20.0),
-              child: Text(
-                  'Benvenuto',
-                style: TextStyle(color: Colors.blue, fontSize: 24.0),
-              ),
-            ),
-            const Padding(
-                padding: EdgeInsets.only(top: 80)
-            ),
-            TextField(
-              controller: usernameController,
-              keyboardType: TextInputType.name,
-              textCapitalization: TextCapitalization.sentences,
-              style: TextStyle(
-                color: correctUsername ? Colors.blue : Colors.red
-              ),
-              decoration: InputDecoration(
-                hintText: 'Inserisci l\'username',
-                hintStyle: TextStyle(color: correctUsername ? Colors.blueGrey : Colors.red),
-                prefixIcon: Icon(Icons.person, color: correctUsername ? Colors.blue : Colors.red),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.0),
-                  borderSide: BorderSide(
-                    width: 1,
-                    color: correctUsername ? Colors.blue : Colors.red
-                  )
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 120.0),
+                child: Text(
+                    'Welcome back',
+                  style: TextStyle(color: Colors.blue, fontSize: 40.0),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(
-                    width: 2,
-                    color: correctUsername ? Colors.blue : Colors.red
-                  )
-                )
-              )
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: TextField(
-                controller: passwordController,
-                keyboardType: TextInputType.text,
+              ),
+              const Padding(
+                  padding: EdgeInsets.only(top: 80)
+              ),
+              TextField(
+                controller: usernameController,
+                keyboardType: TextInputType.name,
                 textCapitalization: TextCapitalization.sentences,
-                obscureText: !passwordToggle,
                 style: TextStyle(
-                    color: correctPassword ? Colors.blue : Colors.red
+                  color: correctUsername ? Colors.blue : Colors.red
                 ),
                 decoration: InputDecoration(
-                    hintText: 'Inserisci la password',
-                    hintStyle: TextStyle(color: correctPassword ? Colors.blueGrey : Colors.red),
-                    prefixIcon: Icon(Icons.lock,  color: correctPassword ? Colors.blue : Colors.red,),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        passwordToggle ? Icons.visibility_off : Icons.visibility,
-                        color: correctPassword ? Colors.blue : Colors.red,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          passwordToggle = !passwordToggle;
-                        });
-                      },
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                        borderSide: BorderSide(
-                            width: 1,
-                            color: correctPassword ? Colors.blue : Colors.red
-                        )
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                        borderSide: BorderSide(
-                            width: 2,
-                            color: correctPassword ? Colors.blue : Colors.red
-                        )
+                  hintText: 'Email',
+                  hintStyle: TextStyle(color: correctUsername ? Colors.blueGrey : Colors.red),
+                  prefixIcon: Icon(Icons.email_outlined, color: correctUsername ? Colors.blue : Colors.red),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: correctUsername ? Colors.blue : Colors.red
                     )
-                ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: correctUsername ? Colors.blue : Colors.red
+                    )
+                  )
+                )
               ),
-            ),
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 80),
-                  child: OutlinedButton.icon(
-                    onPressed: () {
-                      setState(() {
-                        if (checkInputs()) {
-                          welcomeUser();
-                          usernameController.clear();
-                          passwordController.clear();
-                        }
-                      });
-                    },
-                    icon: const Icon(Icons.check, size: 18,),
-                    label: const Text('Login'),
-                    style: OutlinedButton.styleFrom(
-                      elevation: 2.0,
-                      backgroundColor: Colors.white,
-                      side: const BorderSide(color: Colors.blue, width: 1)
-                    ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: TextField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.text,
+                  textCapitalization: TextCapitalization.sentences,
+                  obscureText: !passwordToggle,
+                  style: TextStyle(
+                      color: correctPassword ? Colors.blue : Colors.red
+                  ),
+                  decoration: InputDecoration(
+                      hintText: 'Password',
+                      hintStyle: TextStyle(color: correctPassword ? Colors.blueGrey : Colors.red),
+                      prefixIcon: Icon(Icons.lock_outlined,  color: correctPassword ? Colors.blue : Colors.red,),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          passwordToggle ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                          color: correctPassword ? Colors.blue : Colors.red,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            passwordToggle = !passwordToggle;
+                          });
+                        },
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: correctPassword ? Colors.blue : Colors.red
+                          )
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          borderSide: BorderSide(
+                              width: 2,
+                              color: correctPassword ? Colors.blue : Colors.red
+                          )
+                      )
                   ),
                 ),
-                Column(
-                  children: [
-                    const Padding(padding: EdgeInsets.only(top: 80.0)),
-                    Visibility(
-                      visible: pressed,
-                      child:
-                        Text('Welcome $username'),
-                    ),
-                    Visibility(
-                      visible: pressed,
-                      child:
-                        Text('Password: $password'),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ],
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 80),
+                        child:
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            padding: MaterialStateProperty.all(
+                              const EdgeInsets.symmetric(horizontal: 80, vertical: 7)
+                            ),
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: const BorderSide(color: Colors.blue)
+                              )
+                            )
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              if (checkInputs()) {
+                                welcomeUser();
+                                usernameController.clear();
+                                passwordController.clear();
+                              }
+                            });
+                          },
+                          child: const Text('Login', style: TextStyle(fontSize: 20.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Padding(padding: EdgeInsets.only(top: 80.0)),
+                      Visibility(
+                        visible: pressed,
+                        child:
+                          Text('Welcome $username'),
+                      ),
+                      Visibility(
+                        visible: pressed,
+                        child:
+                          Text('Password: $password'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
